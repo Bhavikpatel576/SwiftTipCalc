9fa95260e764f5f43e00f285517350c142de8a41
Bhavik Patel <Bhavikpatel576@gmail.com>
2017-02-27 20:51:11 -0800

    Updated settings to include background color

--- tipCalculator/SettingsViewController.swift
+++ tipCalculator/SettingsViewController.swift
@@ -11,38 +11,95 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var defaulttipController: UISegmentedControl!
+    //Button Outlets
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    //Slider Outlets
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let defaults = UserDefaults.standard
         let percentIndex = defaults.integer(forKey: "def_t_pct")
-        
-        defaulttipController.selectedSegmentIndex = percentIndex
+            defaulttipController.selectedSegmentIndex = percentIndex
+        //colorUpdate()
+        //updateSliders()
+        view.layer.borderWidth = 5
+        view.layer.cornerRadius = 20
+        view.layer.borderColor = UIColor.black.cgColor
+        //code to update the background color from previous settings
+        redSwitch.isOn = defaults.bool(forKey: "r_switch")
+        greenSwitch.isOn = defaults.bool(forKey: "g_switch")
+        blueSwitch.isOn = defaults.bool(forKey: "b_switch")
+        updateSliders()
+        redSlider.value = defaults.float(forKey: "red_slider_value")
+        greenSlider.value = defaults.float(forKey: "green_slider_value")
+        blueSlider.value = defaults.float(forKey: "blue_slider_value")
+        colorUpdate()
     
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func tipchangeController(_ sender: Any) {
         let defaults = UserDefaults.standard
         let index = defaulttipController.selectedSegmentIndex
-        
-        defaults.set(index, forKey: "def_t_pct")
-        defaults.synchronize()
+            defaults.set(index, forKey: "def_t_pct")
+            defaults.synchronize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func switchChanged(_ sender: UISwitch){
+        colorUpdate()
+        updateSliders()
+        let defaults = UserDefaults.standard
+        defaults.set(redSwitch.isOn, forKey: "r_switch")
+        defaults.set(greenSwitch.isOn, forKey: "g_switch")
+        defaults.set(blueSwitch.isOn, forKey: "b_switch")
+        defaults.synchronize()
+        
     }
-    */
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        colorUpdate()
+    }
+
+    func colorUpdate(){
+        let defaults = UserDefaults.standard
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if (redSwitch.isOn) {
+            red = CGFloat(redSlider.value)
+        }
+        if (greenSwitch.isOn) {
+            green = CGFloat(greenSlider.value)
+        }
+        if (blueSwitch.isOn) {
+            blue = CGFloat(blueSlider.value)
+        }
+
+        defaults.set(red, forKey: "red_slider_value")
+        defaults.set(green, forKey: "green_slider_value")
+        defaults.set(blue, forKey: "blue_slider_value")
+        defaults.synchronize()
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        view.backgroundColor = color
+
+    }
+    
+    func updateSliders(){
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+        //redSlider.value = UserDefaults.standard.float(forKey: "red_slider_value")
+    }
 
 }
--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalBill: UITextField!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipField: UILabel!
+    @IBOutlet weak var colorView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -24,12 +24,14 @@ class ViewController: UIViewController {
         let defaults = UserDefaults.standard
         let tip_amt = defaults.integer(forKey: "def_t_pct")
         tipAmount.selectedSegmentIndex = tip_amt
+        view.backgroundColor = UIColor(red: CGFloat(defaults.float(forKey: "red_slider_value")), green: CGFloat(defaults.float(forKey: "green_slider_value")), blue: CGFloat(defaults.float(forKey: "blue_slider_value")), alpha: 1)
     }
 
     @IBAction func onTap(_ sender: Any) {
         //print hello
         view.endEditing(true)
     }
+    
     @IBAction func Bill(_ sender: AnyObject) {
         let tipPct = [0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
@@ -38,11 +40,5 @@ class ViewController: UIViewController {
         let total = bill + tip
         totalBill.text = String(format: "$%.2f", total)
         tipField.text = String(format: "$%.2f", tip)
-        
     }
-    
-
-
-    
 }
-
